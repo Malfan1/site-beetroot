100cef421a9aab80426329c6bd29d2ff82031768
Your Name <you@example.com>
2023-02-02 19:47:24 +0200

    smth

--- src/js/slick_new-arrives.js
+++ src/js/slick_new-arrives.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
     $(".new-arrives__slider").slick({
-        arrows: false,
         slidesToShow: 4,
+        slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 3000,
+        responsive : [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                }
+            },
+        ]
     });
   });
--- src/js/slider_advantages.js
+++ src/js/slider_advantages.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 4000,
+            autoplaySpeed: 3000,
           }
         },
         {
@@ -18,7 +18,7 @@ $(document).ready(function(){
             slidesToShow: 2,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 4000,
+            autoplaySpeed: 3000,
           }
         }
       ]
