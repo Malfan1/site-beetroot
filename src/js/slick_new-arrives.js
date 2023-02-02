4492f5a7268f6f0639679edf688fc9f8b93132f1
Your Name <you@example.com>
2023-02-02 18:32:18 +0200

    slick new arrives

---
+++ src/js/slick_new-arrives.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".new-arrives__slider").slick({
+        arrows: false,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+  });
