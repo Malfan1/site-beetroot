bab0bbacf715d4ca5c45ebaf3179d31dfea3ee3d
Your Name <you@example.com>
2023-02-04 21:22:11 +0200

    a

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,3 +1,6 @@
-document.getElementById("nav-main-ul").onmouseover = function(event){
+const menu = querySelectorAll(".header-for-992px__nav__ul-menu");
+
+document.querySelectorAll("#nav-main-ul")?.forEach(element => element.onmouseover = function(event){
     console.log(event);
-}
+    menu.classList.add("block");
+})
