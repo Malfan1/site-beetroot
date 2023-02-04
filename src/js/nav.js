e48d4ef6f8db9eb5f3215611a12cbf4baa09daad
Your Name <you@example.com>
2023-02-04 23:45:39 +0200

    h

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,3 @@
-const menu = querySelectorAll(".header-for-992px__nav__ul-menu");
-
 document.querySelectorAll("#nav-main-ul")?.forEach(element => element.onmouseover = function(event){
     console.log(event);
-    menu.classList.add("block");
 })
--- src/js/popup_login.js
+++ src/js/popup_login.js
@@ -1,14 +1,18 @@
-const openPopUp = document.querySelector("#open-pop-up-login");
-const container = document.querySelector('#pop-up-login-container');
-
-openPopUp.addEventListener('click', function(e){
-    //e.preventDefault();
-    container.classList.add("active");
+$('#open-pop-up-login').click( function() {
+    $('#pop-up-login-container').fadeToggle();
 });
 
-document.addEventListener('click', (e) =>{
-    const click = e.composedPath().includes(openPopUp);
-    if (!click){
-        container.classList.remove("active");
+
+$(document).mouseup( function(e){ // событие клика по веб-документу
+    var div = $( "#pop-up-login-container" ); // тут указываем ID элемента
+    if ( !div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+        div.fadeOut(); // скрываем его
     }
-})
+});
+
+$(document).on('keyup', function(e) {
+	if ( e.key == "Escape" ) {
+		$( "#pop-up-login-container" ).fadeOut();
+	}
+});
