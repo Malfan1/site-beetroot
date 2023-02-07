365a512d8ae23157593b07d5a4a65018cb247c5f
Your Name <you@example.com>
2023-02-07 03:11:35 +0200

    a12

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,7 +1,39 @@
-document.querySelectorAll("#nav-main-ul")?.forEach(element => element.onmouseover = function(event){
-    console.log(event);
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtns = document.querySelectorAll('.header-for-992px__nav__ul__li_span');
+    const drops = document.querySelectorAll('.header-for-992px__nav__ul-menu');
 
-document.querySelectorAll("#open-pop-up-login")?.forEach(element => element.onclick = function(event){
-    
-})
+    menuBtns.forEach(el => {
+        el.addEventListener('click', (e) => {
+            let currentBtn = e.currentTarget;
+            let drop = currentBtn.closest('.header-for-992px__nav__ul__li').querySelector('.header-for-992px__nav__ul-menu');
+
+
+            menuBtns.forEach(el => {
+                if(el !== currentBtn){
+                    el.classList.remove('activee');
+                }
+            });
+
+            drops.forEach(el => {
+                if(el !== drop){
+                    el.classList.remove('grid-header')
+                }
+            })
+
+            drop.classList.toggle('grid-header')
+            currentBtn.classList.toggle('activee');
+        })
+    })
+
+    document.addEventListener('click', (e) => {
+        if(!e.target.closest('.header-for-992px__nav')){
+            menuBtns.forEach(el => {
+                el.classList.remove('activee');
+                
+            });
+            drops.forEach(el => {
+                el.classList.remove('grid-header')
+            })
+        }
+    })
+});
--- src/js/popup_login.js
+++ src/js/popup_login.js
@@ -3,14 +3,6 @@ $('#open-pop-up-login').click( function() {
 });
 
 
-/* $(document).mouseup( function(e){ // событие клика по веб-документу
-    var div = $( "#pop-up-login-container" ); // тут указываем ID элемента
-    if ( !div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
-        div.fadeOut(); // скрываем его
-    }
-}); */
-
 $(document).on('keyup', function(e) {
 	if ( e.key == "Escape" ) {
 		$( "#pop-up-login-container" ).fadeOut();
